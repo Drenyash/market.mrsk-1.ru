9821fbe0cb121ba59fb7c8dca840d69248a8c3dc
TakenHara <kavlod12345@gmail.com>
2024-01-24 15:25:57 +0200

    feat: Скопировал проект из основного гита

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import "./app/select";
+import "./app/form";
+import "./app/modals";
+import "./app/tel-mask";
+import "./app/tabs";
+import "./app/header-menu";
+import "./app/fancybox";
+import "./app/slider";
+import "./app/text-writing";
