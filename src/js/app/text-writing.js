a00ec1e87d71322f10f2787e1fd5be8ec321cf3a
TakenHara <kavlod12345@gmail.com>
2024-02-04 13:30:43 +0200

    feat: доработал скрипт анимированного счётчика

--- src/js/app/text-writing.js
+++ src/js/app/text-writing.js
@@ -1,19 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   const textItems = document.querySelectorAll("[data-text-writing]");
+  let longestEl = null;
   textItems.forEach((item) => {
-    new TextWriting(item);
+    let itemCountLength = item.getAttribute("data-text-writing").length
+    if (itemCountLength > longestEl) {
+      longestEl = itemCountLength;
+    }
+  });
+  textItems.forEach((item) => {
+    new TextWriting(item, longestEl);
   });
 });
 
 class TextWriting {
-  constructor(text) {
+  constructor(text, longestValue) {
     this.block = text;
     this.text = text.getAttribute("data-text-writing");
-    this.speed = 1000;
+    this.speed = 500;
     this.position = this.block.getBoundingClientRect();
     this.iterator = 1;
-    this.interval = null;
-
+    this.totalLength = longestValue;
     this.setListener();
   }
 
@@ -22,21 +28,20 @@ class TextWriting {
   }
 
   typeWriter() {
-    if (this.text) {
-      setInterval(() => {
-        if (this.iterator <= this.text.length) {
-          this.block.innerHTML = this.text.substr(0, this.iterator) + "<span class='line'>|</span>";
-          this.iterator += 1;
-        } else {
-          this.iterator = 1;
-        }
-      }, this.speed);
-      setInterval(() => {
-        let line = this.block.querySelector("span");
-        if (line) {
-          line.classList.contains('animate') ? line.classList.remove('animate') : line.classList.add('animate');
-        }
-      }, 400);
-    }
+    if (!this.text) return;
+    setInterval(() => {
+      if (this.iterator <= this.totalLength) {
+        this.block.innerHTML = this.text.substr(0, this.iterator) + "<span class='line'>|</span>";
+        this.iterator += 1;
+      } else {
+        this.iterator = 1;
+      }
+    }, this.speed);
+    setInterval(() => {
+      const line = this.block.querySelector("span");
+      if (line) {
+        line.classList.contains("animate") ? line.classList.remove("animate") : line.classList.add("animate");
+      }
+    }, 400);
   }
 }
